d8ed9000e4ef48e24c3e90e66743a065198ae90b
ramsir3 <ramsir3@gmail.com>
2018-10-28 00:11:07 -0600

    restructured game class slightly

--- game.py
+++ game.py
@@ -4,27 +4,31 @@ from world import world
 from entity import player
 from constants import WINDOW_SIZE, DIRECTION, CONTROLS
 
-pg.init()
+def init():
+    pg.init()
 
-w = world('assets/level.dat')
-p = player(pos=[3,3], world=w)
-screen = pg.display.set_mode(WINDOW_SIZE)
+    w = world('assets/level.dat')
+    p = player(pos=[3,3], world=w)
+    screen = pg.display.set_mode(WINDOW_SIZE)
 
-def update():
-    for e in pg.event.get():
-        if e.type == pg.QUIT: sys.exit()
-        if e.type == pg.KEYDOWN:
-            if e.key in CONTROLS:
-                p.move(CONTROLS[e.key])
-                print(p.pos)
+    def update():
+        for e in pg.event.get():
+            if e.type == pg.QUIT: sys.exit()
+            if e.type == pg.KEYDOWN:
+                if e.key in CONTROLS:
+                    p.move(CONTROLS[e.key])
+                    print(p.pos)
 
-def draw():
-    w.draw(screen)
-    p.draw(screen)
-    pg.display.flip()
-    pass    
+    def draw():
+        w.draw(screen)
+        p.draw(screen)
+        pg.display.flip()  
 
-while 1:       
-    update()
-    draw()
+    return update, draw
+
+if __name__ == '__main__':
+    update, draw = init()
+    while 1:       
+        update()
+        draw()
 
